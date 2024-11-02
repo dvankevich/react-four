345b7c75c096d5d0ea8b72297faf2bc735f3b2c6
Dmitry <dvankevich@gmail.com>
2024-11-02 09:36:53 +0200

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
 import axios from "axios";
 import './App.css'
@@ -16,13 +17,25 @@ const ArticleList = ({ items }) => (
 
 const App = () => {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchArticles() {
-      const response = await axios.get(
-        "https://hn.algolia.com/api/v1/search?query=react"
-      );
-      setArticles(response.data.hits);
+      try {
+		// 1. Встановлюємо індикатор в true перед запитом
+        setLoading(true);
+        const response = await axios.get(
+          "https://hn.algolia.com/api/v1/search?query=react"
+        );
+        setArticles(response.data.hits);
+      } catch (error) {
+        // Тут будемо обробляти помилку
+        console.log(error);
+        
+      } finally {
+		// 2. Встановлюємо індикатор в false після запиту
+        setLoading(false);
+      }
     }
 
     fetchArticles();
@@ -31,9 +44,11 @@ const App = () => {
   return (
     <div>
       <h1>Latest articles</h1>
+      {loading && <p>Loading data, please wait...</p>}
       {articles.length > 0 && <ArticleList items={articles} />}
     </div>
   );
 };
+
 
 export default App
